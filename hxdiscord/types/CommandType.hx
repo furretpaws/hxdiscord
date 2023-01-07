c2150b690d4763e5592cb632d388681a1427af3a
Furret <furretpro@gmail.com>
2023-01-07 20:00:53 +0100

    :(

---
+++ hxdiscord/types/CommandType.hx
@@ -0,0 +1,15 @@
+package hxdiscord.types;
+
+class CommandType {
+    public static var SUB_COMMAND:Int = 1;
+    public static var SUB_COMMAND_GROUP:Int = 2;
+    public static var STRING:Int = 3;
+    public static var INTEGER:Int = 4;
+    public static var BOOLEAN:Int = 5;
+    public static var USER:Int = 6;
+    public static var CHANNEL:Int = 7;
+    public static var ROLE:Int = 8;
+    public static var MENTIONABLE:Int = 9;
+    public static var NUMBER:Int = 10;
+    public static var ATTACHMENT:Int = 11;
+}
