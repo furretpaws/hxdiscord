82f0a5719bd9a4831f4a195f3ce0ab2fb7daa2e4
furretpaws <99131021+FurretDev@users.noreply.github.com>
2024-04-21 22:56:21 +0200

    Update Endpoints.hx

--- hxdiscord/endpoints/Endpoints.hx
+++ hxdiscord/endpoints/Endpoints.hx
@@ -170,8 +170,8 @@ class Endpoints
         @param id The ID of the user
     **/
 
-    public static function getGuildMembers(gid:String):Dynamic {
-	    var r = new Http("https://discord.com/api/v"+Gateway.API_VERSION+"/guilds/" + gid + "/members");
+    public static function getGuildMembers(gid:String, limit:Int = 0, after:Int = 0):Dynamic {
+	    var r = new Http("https://discord.com/api/v"+Gateway.API_VERSION+"/guilds/" + gid + "/members?limit="+limit+"&after="+after);
         //trace("https://discord.com/api/v"+Gateway.API_VERSION+"/users/" + id);
 
         r.addHeader("User-Agent", "hxdiscord (https://github.com/FurretDev/hxdiscord)");
